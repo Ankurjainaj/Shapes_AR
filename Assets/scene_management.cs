96e6d56092e6f3cd444d9dfb8e72ec11d65e6cb6
Ankur Jain <ankurjainaj1234@gmail.com>
2019-01-28 22:43:18 +0530

    First commit

---
+++ Assets/scene_management.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class scene_management : MonoBehaviour {
+
+	public void load()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1);
+    }
+    public void exit()
+    {
+        Application.Quit();
+    }
+    public void back()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+
+}
